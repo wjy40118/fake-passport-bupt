e5306d1d6eea75fa2c6ed7818d2863378f91bd0e
Charlie Chiang <charlie_c_0129@outlook.com>
2021-08-02 13:27:39 +0800

    feat: use random information

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express   = require('express')
 const fs        = require('fs');
 const path      = require('path')
+const faker     = require('faker/locale/zh_CN')
 const app       = express()
 const port      = process.env.PORT || 10985
 const staticRes = express.static('static')
+
+const schoolList = [
+  '信息与通信工程学院',
+  '电子工程学院',
+  '计算机学院（国家示范性软件学院）',
+  '网络空间安全学院',
+  '人工智能学院',
+  '现代邮政学院（自动化学院）',
+  '经济管理学院',
+  '理学院',
+  '人文学院',
+  '数字媒体与设计艺术学院',
+  '马克思主义学院',
+  '国际学院'
+]
+
+const getRandomSchool = () => {
+  const randomIndex = Math.floor(Math.random() * schoolList.length)
+  return schoolList[randomIndex]
+}
+
+const getRandomName = () => {
+  return faker.name.lastName() + faker.name.firstName()
+}
+
+const getRandomId = () => {
+  // 2019 2119 15
+  const currentYear = (new Date()).getFullYear()
+  const randomYear = Math.floor(Math.random() * (currentYear - 2017)) * 1000000 + 2017000000
+  const randomId = Math.floor(Math.random() * 1000000) + 1000000
+  return randomYear + randomId
+}
 
 app.get("/", function (req, res) {
   fs.readFile(path.join(__dirname, 'static', 'index.html'), function (err, data) {
@@ -13,10 +46,10 @@ app.get("/", function (req, res) {
       let htmlString = data.toString()
       const date     = new Date(Date.now() + 8 * 60 * 60 * 1000)
       htmlString     = htmlString
-        .replace('__name__', req.query?.name || '吴铭实')
-        .replace('__school__', req.query?.school || '计算机学院（国家示范性软件学院）')
+        .replace('__name__', req.query?.name || getRandomName())
+        .replace('__school__', req.query?.school || getRandomSchool())
         .replace('__type__', req.query?.type || '入')
-        .replace('__id__', req.query?.id || '2019211915')
+        .replace('__id__', req.query?.id || getRandomId())
         .replace('__time__', date.toISOString().replace("T", " ").slice(0, -5))
       res.setHeader('Content-Type', 'text/html')
       res.send(htmlString);
